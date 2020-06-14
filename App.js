1c5793e4df55dfbd0475db25415f6fa2a2126bdc
Abundiz <3duabundiz@gmail.com>
2020-06-14 14:52:24 -0500

    Agregando primeras vistas y usar el context y userReducer

--- App.js
+++ App.js
@@ -6,29 +6,81 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import NuevaOrden from './views/NuevaOrden';
 import Menu from './views/Menu';
-import DetallePedido from './views/DetallePedido';
-import FormularioPedido from './views/FormularioPedido';
+import DetallePlatillo from './views/DetallePlatillo';
+import FormularioPlatillo from './views/FormularioPlatillo';
 import ResumenPedido from './views/ResumenPedido';
 import ProgresoPedido from './views/ProgresoPedido';
 
+//importar state de context
+import FirebaseState from './context/firebase/firebaseState';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="Nueva Orden"
-        component={NuevaOrden}
-        options={{
-          title:"Nueva Orden"
-        }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-     
+    <FirebaseState>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle:{
+              backgroundColor:"#FFDA00"
+            },
+            headerTitleStyle:{
+              fontWeight:'bold'
+            }
+          }}
+        >
+          <Stack.Screen 
+          name="Nueva Orden"
+          component={NuevaOrden}
+          options={{
+            title:"Nueva Orden"
+          }}
+          />
+
+          <Stack.Screen 
+          name="Menun"
+          component={Menu}
+          options={{
+            title:"Nuestro Menu"
+          }}
+          />
+
+          <Stack.Screen 
+          name="DetallePlatilo"
+          component={DetallePlatillo}
+          options={{
+            title:"Detalle Platillo"
+          }}
+          />
+
+          <Stack.Screen 
+          name="FormilarioPlatillo"
+          component={FormularioPlatillo}
+          options={{
+            title:"Ordenar Platillo"
+          }}
+          />
+
+          <Stack.Screen 
+          name="ResumenPedido"
+          component={ResumenPedido}
+          options={{
+            title:"Resumen Pedido"
+          }}
+          />
+
+          <Stack.Screen 
+          name="ProgresoPedido"
+          component={ProgresoPedido}
+          options={{
+            title:"Progreso del pedido"
+          }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FirebaseState>
     </>
   );
 };
---
+++ views/FormularioPlatillo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+const  FormularioPlatillo = () =>{
+    return( 
+        <Text>Formulario Platillo</Text>
+    );
+}
+
+export default FormularioPlatillo ;
