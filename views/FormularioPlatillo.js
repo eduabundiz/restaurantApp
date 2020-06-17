26ccdfa9774f7a35c620e6ac0b47f600bda40d81
Abundiz <3duabundiz@gmail.com>
2020-06-17 10:56:59 -0500

    agregando los botones + -

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,9 +1,60 @@
 import React from 'react';
-import {Text,View} from 'react-native';
+import {StyleSheet,View} from 'react-native';
+import { 
+    Container,
+    Content,
+    Form,
+    Icon,
+    Input,
+    Grid,
+    Text,
+    Col,
+    Button,
+    
+} from 'native-base';
+import { useNavigation} from '@react-navigation/native';
+import globalStyle from '../styles/global';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
 
 const  FormularioPlatillo = () =>{
     return( 
-        <Text>Formulario Platillo</Text>
+        <Container>
+            <Content>
+                <Form>
+                    <Text style={globalStyle.titulo}>Cantidad</Text>
+                    <Grid>
+                        <Col>
+                            <Button
+                                props
+                                dark
+                                style={{height:80,justifyContent:'center'}}
+                            >
+                                <Icon style={{fontSize:40}} name="remove" />
+                            </Button>
+                        </Col>
+
+                        <Col>
+                            <Input 
+                                style={{textAlign:'center',fontSize:20}}
+
+                                value="1"
+                            />
+                        </Col>
+
+                        <Col>
+                            <Button
+                                props
+                                dark
+                                style={{height:80,justifyContent:'center'}}
+                            >
+                                <Icon style={{fontSize:40}} name="add" />
+                            </Button>                         
+                        </Col>
+                    </Grid>
+                </Form>
+            </Content>
+        </Container>
     );
 }
 
