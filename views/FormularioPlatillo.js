4f2fa4caa954f60ed176cfdc19bc503f4aab10e9
Abundiz <3duabundiz@gmail.com>
2020-06-17 11:07:04 -0500

    Incremento y decremento de cantidad con los botones

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {StyleSheet,View} from 'react-native';
 import { 
     Container,
@@ -18,6 +18,24 @@ import globalStyle from '../styles/global';
 import PedidoContext from '../context/pedidos/pedidosContext';
 
 const  FormularioPlatillo = () =>{
+
+    //State para cantidades
+    const [ cantidad, guardarCantidad]  = useState(1);
+    
+    //Decrementar en uno la cantidad
+    const decrementarUno = () =>{
+        if( cantidad > 1){
+            const nuevaCantidad = parseInt(cantidad) -1
+            guardarCantidad(nuevaCantidad)
+        }
+    }
+
+    //Incrementar en uno la cantidad
+    const incrementarUno = () =>{
+        const nuevaCantidad = parseInt(cantidad) +1
+        guardarCantidad(nuevaCantidad)
+    }
+
     return( 
         <Container>
             <Content>
@@ -29,6 +47,7 @@ const  FormularioPlatillo = () =>{
                                 props
                                 dark
                                 style={{height:80,justifyContent:'center'}}
+                                onPress={ () => decrementarUno()}
                             >
                                 <Icon style={{fontSize:40}} name="remove" />
                             </Button>
@@ -37,8 +56,9 @@ const  FormularioPlatillo = () =>{
                         <Col>
                             <Input 
                                 style={{textAlign:'center',fontSize:20}}
-
-                                value="1"
+                                value={cantidad.toString()}
+                                keyboardType='numeric'
+                                onChangeText={ cantidad => guardarCantidad(cantidad)}
                             />
                         </Col>
 
@@ -47,6 +67,7 @@ const  FormularioPlatillo = () =>{
                                 props
                                 dark
                                 style={{height:80,justifyContent:'center'}}
+                                onPress={ () => incrementarUno()}
                             >
                                 <Icon style={{fontSize:40}} name="add" />
                             </Button>                         
